f4d3d3555c4ef405fa90580073cb06a456c9b41d
pavelbesedin <pavelbesedin97@mail.ru>
2023-03-20 19:28:05 +0300

    Task 1 С помощью цикла for выведите в консоль все числа от 1 до 10.

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,12 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
-//        ... // Пишем код для задачи 1
+//        С помощью цикла for выведите в консоль все числа от 1 до 10.
+
+        for(int i = 1; i < 11; i++)
+            System.out.println("Число " + i);
     }
+
 
     public static void task2 () {
         System.out.println("Задача 2");
